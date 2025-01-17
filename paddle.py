cfc1983010928f3db85006ff897d751832a6920d
Ryan Guenther <ryan.guenther@gmail.com>
2025-01-17 12:34:13 -0700

    Implementation of the paddles with movement

---
+++ paddle.py
@@ -0,0 +1,27 @@
+import turtle
+
+COLOR = "white"
+SHAPE = "square"
+MOVEMENT = 20
+SPEED = "fastest"
+
+class Paddle(turtle.Turtle):
+    def __init__(self, position):
+        super().__init__(SHAPE)
+
+        self.shapesize(stretch_wid = 5, stretch_len=1, outline=None)
+        self.color(COLOR)
+        self.penup()
+        self.speed(SPEED)
+
+        self.setpos(position)
+
+
+    def move_up(self):
+        new_ycor = self.ycor() + MOVEMENT
+        self.setpos(self.xcor(), new_ycor)
+
+
+    def move_down(self):
+        new_ycor = self.ycor() + MOVEMENT
+        self.setpos(self.xcor(), self.ycor() - MOVEMENT)
